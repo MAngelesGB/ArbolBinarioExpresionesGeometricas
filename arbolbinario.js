325ea809883a21cc1ec4167a24e67210e8939140
MAngelesGB <mgutierrez6@ucol.mx>
2022-11-27 15:06:28 -0600

    arbol

--- arbolbinario.js
+++ arbolbinario.js
@@ -18,7 +18,7 @@ class ArbolBinario{
 
     agregar(nuevo)
     {
-        if(!this.primero)
+        if(this.primero === null)
         {
             this.primero = nuevo; 
             this.ultimo = nuevo; 
@@ -28,11 +28,96 @@ class ArbolBinario{
             this.ultimo.sig=nuevo;
             nuevo.ant=this.ultimo;
             this.ultimo=nuevo;
-        }               
-    } 
+        }        
+    }
+    
+    recorrer()
+    {
+        let temp = this.primero;
+        let arbol = null; 
+
+        while(temp !== null)
+        {
+            if(temp.dato === '*' || temp.dato === '/')
+            {   
+                temp.hizq = temp.ant; 
+                temp.hder = temp.sig;
+
+                if(temp.ant.ant === null && temp.sig.sig === null)
+                {
+                    temp.sig = null; 
+                    temp.ant =  null; 
+                }
+                else if(temp.sig.sig === null)
+                {
+                    temp.sig = null; 
+                    temp.ant = temp.ant.ant; 
+                    temp.ant.sig = temp; 
+                }
+                else if(temp.ant.ant === null)
+                {
+                    temp.ant = null;
+                    temp.sig = temp.sig.sig;
+                    temp.sig.ant = temp; 
+                }
+                else 
+                {
+                    temp.sig = temp.sig.sig; 
+                    temp.ant = temp.ant.ant; 
+                    temp.ant.sig = temp; 
+                    temp.sig.ant = temp; 
+                }
+            }
+
+            temp = temp.sig; 
+        } 
+        
+        temp = this. primero; 
+        
+        while(temp !== null)
+        {
+            if(temp.dato === '+' || temp.dato === '-')
+            {   
+                temp.hizq = temp.ant; 
+                temp.hder = temp.sig;
+
+                if(temp.ant.ant === null && temp.sig.sig === null)
+                {
+                    temp.sig = null; 
+                    temp.ant =  null; 
+                }
+                else if(temp.sig.sig === null)
+                {
+                    temp.sig = null; 
+                    temp.ant = temp.ant.ant; 
+                    temp.ant.sig = temp; 
+                }
+                else if(temp.ant.ant === null)
+                {
+                    temp.ant = null;
+                    temp.sig = temp.sig.sig;
+                    temp.sig.ant = temp; 
+                }
+                else 
+                {
+                    temp.sig = temp.sig.sig; 
+                    temp.ant = temp.ant.ant; 
+                    temp.ant.sig = temp; 
+                    temp.sig.ant = temp; 
+                }
+                arbol = temp; 
+            }
+
+            temp = temp.sig; 
+        } 
+        
+        this.primero = arbol; 
+        console.log(this.primero);
+
+    }
 }
 
-let expresion = '4-2+3*5-8*3/6';
+let expresion = '2*8+4+3-2*9/6-3*4/2/2';
 let array = Array.from(expresion);
 let nodo = new Nodo();
 let arbol = new ArbolBinario(); 
@@ -43,4 +128,4 @@ for(let i = 0; i < array.length; i++)
     arbol.agregar(nodo); 
 }
 
-console.log(arbol.listar()); 
+arbol.recorrer();
